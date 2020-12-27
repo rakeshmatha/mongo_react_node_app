a18f49d12e3045ebaabba3f1008f898d25bc1ae8
Rocky <matharakesh46@gmail.com>
2020-12-27 04:07:22 -0500

    dependencies

---
+++ backend/routes/exercises.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const e = require('express');
+let Exercise = require('../models/excercise.model');
+
+router.route('/').get((req, res) =>{
+    Exercise.find()
+        .then( excercises => res.json(excercises))
+        .catch(err => res.status(400).json('Erroe:' + err))
+});
+
+router.route('/add').post((req,res) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExcercise = new Exercise({
+        username,
+        description,
+        duration,
+        date,
+    });
+
+    newExcercise.save()
+        .then(() => res.json('Excercise added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+    router.route('/:id').get((req , res) => {
+        Exercise.findById(req.params.id)
+            .then(excercise => res.json(excercise))
+            .catch(err => res.status(400).json('Erroe:' + err));
+    });
+
+    router.route('/:id').delete((req, res) => {
+        Exercise.findByIdAndDelete(req.params.id)
+            .then(() => res.json('Excercise deleted.'))
+            .catch(err => res.status(400).json('Error :' + err));
+    });
+
+    router.route('/update/:id').post((req , res) => {
+        Exercise.findById(req.params.id)
+            .then(excercise => {
+                excercise.username = req.body.username;
+                excercise.description = req.body.description;
+                excercise.duration = Number(req.body.duration);
+                excercise.date = Date.parse(req.body.date);
+            
+            excercise.save()
+                .then(() => res.json('Excercise updated!'))
+                .catch(err => res.status(400).json('Error: ')+ err);
+            })
+            .catch(err => res.status(400).json('Error: '+err));
+    });
+
+    module.exports = router;
